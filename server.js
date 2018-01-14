b60e75767d1ae1894b2c3b37d8c80f91d149cbec
Gregory Schnese <theschnaz@gmail.com>
2018-01-14 14:22:42 -0500

    tweaking server file, trying to fix heroku issue

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
